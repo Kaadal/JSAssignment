360eb57abad9487d06e3fd14d3a9c56afba9971e
Kaadal <kaadal012@ihmeducation.se>
2024-09-12 11:35:09 +0200

    UpdateVariables

---
+++ P4Variables/js/app.js
@@ -0,0 +1,12 @@
+let numberOfBottles = 10
+const line1 ="green bottles haning on the wall";
+const line2 = "and if one green bottle, should accidental fall, there will be"
+
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+console.log(line1);
+console.log(line2);
+console.log(--numberOfBottles);
+//--numberOfBottles
+console.log(line1);
