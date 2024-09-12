da571bb543da7678802100e7fb9d16bc11063c4f
Kaadal <kaadal012@ihmeducation.se>
2024-09-12 15:30:02 +0200

    Mathematics part 1&2

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -7,3 +7,29 @@ let minutesInput = 3;
 let secondsOutput = 0;
 secondsOutput = minutesInput * 60;
 console.log(secondsOutput);
+
+let xInput = 11;
+let yInput = 4;
+let totalOutput = xInput/yInput
+console.log(totalOutput)
+
+console.log(xInput % yInput)
+
+let radius = 2
+let area = 3.14
+console.log(area*radius**2);
+
+let number = 11
+let negation = -number;
+console.log(negation);
+
+let weight =70
+let height = 1.82
+console.log(weight/height**2)
+
+let aside = 3;
+let bside = 4;
+let hypotenuse= Math.sqrt(aside**2 + bside**2);
+console.log(hypotenuse)
+
+
