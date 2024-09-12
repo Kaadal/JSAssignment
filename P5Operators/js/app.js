f76a1893414a58618818df8acf5684b03064d4bc
Kaadal <kaadal012@ihmeducation.se>
2024-09-12 14:52:02 +0200

    Add Assignment Structure

---
+++ P5Operators/js/app.js
@@ -0,0 +1,9 @@
+let speedInput = 72;
+let speedOutput = 0;
+speedOutput = speedInput / 3.6;
+console.log(speedOutput);
+
+let minutesInput = 3;
+let secondsOutput = 0;
+secondsOutput = minutesInput * 60;
+console.log(secondsOutput);
