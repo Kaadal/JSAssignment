6e22061c09daa4147351a8adb1e49f9c9163df85
Kaadal <kaadal012@ihmeducation.se>
2024-09-12 14:42:52 +0200

    Create app2

---
+++ P5Operators/js/app2.js
@@ -0,0 +1,45 @@
+/*let firstNumber = 10;
+let secondNumber = 20;
+let answer = 0;
+// 1+ 2 = 3
+answer = firstNumber + secondNumber;
+console.log(answer);
+
+console.log(firstNumber - secondNumber);
+console.log(answer);
+
+answer = firstNumber / secondNumber
+console.log(answer);
+
+answer = firstNumber * secondNumber
+console.log(answer);
+
+anser = firstNumber % secondNumber;
+console.log(answer);
+*/
+
+/*
+let gold = 50
+gold + 10
+console.log(gold);
+
+gold = gold + 10;
+console.log(gold);
+
+gold += 10;
+console.log(gold);
+
+gold *= 10;
+console.log(gold);
+
+gold -= 10;
+console.log(gold);
+
+gold %= 10;
+console.log(gold);
+*/
+
+let numberOfStudents = 0;
+
+numberOfStudents ++;
+console.log(numberOfStudents);
