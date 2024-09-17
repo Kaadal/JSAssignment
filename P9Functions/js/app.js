99f292706f7a058c6d7bc93b3ec8cfd6bb4bcd92
Kaadal <kaadal012@ihmeducation.se>
2024-09-17 09:22:14 +0200

    Update app.js

---
+++ P9Functions/js/app.js
@@ -0,0 +1,37 @@
+
+walkThreeSteps()
+turnAround();
+walkThreeSteps()
+turnLeft()
+stepForward()
+turnAround()
+stepForward()
+
+
+function stepForward() {
+ console.log ('Move Forward. ');
+}
+
+ function turnRight() {
+ console.log ('Turn right. ');
+}
+
+  function turnLeft() {
+  turnRight()
+  turnRight()
+  turnRight()
+   }
+
+   function turnAround() {
+
+
+     turnRight()
+     turnRight()
+   }
+   function walkThreeSteps() {
+  stepForward()
+     stepForward()
+     stepForward()
+   }
+
+
